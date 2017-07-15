dff83d7f1e016e34dfc81ef89cad19131bc36a09
Ramon J. A. Smit <ramon@daltcore.com>
2017-07-15 20:37:14 +0200

    Create CheckRole.php

---
+++ src/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DALTCORE\Permissions\Http\Middleware;
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $permission)
+    {
+
+        if(!$request->user()->hasRole($permission))
+        {
+            return redirect()->route('cms.dashboard')->withError('You do not have the role '. $permission);
+        }
+
+        return $next($request);
+    }
+}
