1dde547e5e1956d0f42211ab70b22a91a47ef60d
Ramon J. A. Smit <ramon@daltcore.com>
2017-07-15 20:36:59 +0200

    Create CheckPermission.php

---
+++ src/Http/Middleware/CheckPermission.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DALTCORE\Permissions\Http\Middleware;
+
+use Closure;
+
+class CheckPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $permission)
+    {
+
+        if(!$request->user()->hasPermission($permission))
+        {
+            return redirect()->route('cms.dashboard')->withError('You do not have the permission '. $permission);
+        }
+
+        return $next($request);
+    }
+}
