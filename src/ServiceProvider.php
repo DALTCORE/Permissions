bdeb67a0dbe60d42228cbf217ee2e4d57a2a7bea
Ramon J. A. Smit <ramon@daltcore.com>
2017-07-15 20:50:20 +0200

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,16 +4,10 @@ namespace DALTCORE\Permissions;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
-/**<?php
-
-namespace DALTCORE\Permissions;
-
-use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-
 /**
- * Class PackageServiceProvider
+ * Class ServiceProvider
  *
- * @package Daltcore\Skeleton
+ * @package DALTCORE\Permissions
  * @see     http://laravel.com/docs/master/packages#service-providers
  * @see     http://laravel.com/docs/master/providers
  */
@@ -88,69 +82,6 @@ class ServiceProvider extends BaseServiceProvider
         {
             return "<?php endif; ?>";
         });
-    }
-
-    /**
-     * Loads a path relative to the package base directory
-     *
-     * @param string $path
-     *
-     * @return string
-     */
-    protected function packagePath($path = '')
-    {
-        return sprintf("%s/../%s", __DIR__, $path);
-    }
-}
-
- * Class PackageServiceProvider
- *
- * @package Daltcore\Skeleton
- * @see     http://laravel.com/docs/master/packages#service-providers
- * @see     http://laravel.com/docs/master/providers
- */
-class ServiceProvider extends BaseServiceProvider
-{
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @see http://laravel.com/docs/master/providers#deferred-providers
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * Register the service provider.
-     *
-     * @see http://laravel.com/docs/master/providers#the-register-method
-     * @return void
-     */
-    public function register()
-    {
-    }
-
-    /**
-     * Application is booting
-     *
-     * @see http://laravel.com/docs/master/providers#the-boot-method
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerMigrations();
-    }
-
-    /**
-     * Register the package migrations
-     *
-     * @see http://laravel.com/docs/master/packages#publishing-file-groups
-     * @return void
-     */
-    protected function registerMigrations()
-    {
-        $this->publishes([
-            $this->packagePath('database/migrations') => database_path('/migrations')
-        ], 'dpm-migrations');
     }
 
     /**
